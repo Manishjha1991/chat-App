64f7d344afc27adc2fea09b5055e7d5d40cfde74
Manish <manish@Manishs-MacBook-Pro.local>
2017-06-30 19:05:11 +0530

    initial commit

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+var socket = io();
+socket.on('connect',function(){
+  console.log("connected to server");
+socket.on('disconnect',function(){
+  console.log("Disconnected from server");
+})
+socket.on('newMessage',function(msg){
+  console.log("Newmessage",msg);
+})
--- server/server.js
+++ server/server.js
@@ -10,13 +10,18 @@ var server=http.createServer(app);
 var io = socketIO(server);
 io.on('connection',(socket) =>{
   console.log("New User Connected");
-
-
+  socket.on('createMessage',(message)=>{
+    console.log("createMessage",message);
+    io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createAt:new Date().getTime()
+    })
+  })
   socket.on('disconnect',()=>{
     console.log("user is disconnect");
   })
 })
-//  var server =http.creatServer((req,res)=>)
 server.listen(port, () => {
   console.log(`server Running on $(port)`);
 });
