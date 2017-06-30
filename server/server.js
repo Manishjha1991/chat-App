1d13c03200c122171d049f106d7a86a774064696
Manish <manish@Manishs-MacBook-Pro.local>
2017-06-30 14:08:47 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 const path =require("path");
 const  express =require("express");
+const http =require("http");
+const socketIO =require("socket.io");
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app =express();
 app.use(express.static(publicPath));
-app.listen(port, () => {
+var server=http.createServer(app);
+var io = socketIO(server);
+io.on('connection',(socket) =>{
+  console.log("New User Connected");
+
+
+  socket.on('disconnect',()=>{
+    console.log("user is disconnect");
+  })
+})
+//  var server =http.creatServer((req,res)=>)
+server.listen(port, () => {
   console.log(`server Running on $(port)`);
 });
